d31fc2a8b5beab3a21854b21210cf3eb14294d19
Daniel Natta <Daniel.Natta@outlook.com>
2020-04-07 11:47:03 -0400

    with discount on one item

--- src/main/java/com/fdmgroup/util/PriceCalculator.java
+++ src/main/java/com/fdmgroup/util/PriceCalculator.java
@@ -5,10 +5,27 @@ import com.fdmgroup.model.Item;
 
 public class PriceCalculator {
 	public static double getBasketPrice(Basket basket) {
-		double basketPrice = basket.getBasketList()
-		.stream()
-		.mapToDouble(e -> ((Item)e).getPrice())
-		.sum();
-		return basketPrice;
+//		double basketPrice = basket.getBasketList()
+//		.stream()
+//		.mapToDouble(e -> ((Item)e).getPrice())
+//		.sum();
+//		return basketPrice;
+		
+		double minPrice = Double.MAX_VALUE;
+		double sum = 0 ;
+		for(Item i : basket.getBasketList()) {
+			if(i.getPrice()<minPrice) {
+				minPrice=i.getPrice();
+			}
+			sum+=i.getPrice();	
+		}
+		double totalPrice = sum-minPrice;
+		//To cover the case when only 1 item is purchased
+		//or this discount should not be included for just one purchase!
+		//In which case we must do a check to see if basket.getBasketList().size()>2
+		if(totalPrice < 0) {
+			return 0;
+		}
+		return totalPrice;
 	}
 }
--- src/test/java/com/fdmgroup/CheckoutCalculator/PriceCalculatorTest.java
+++ src/test/java/com/fdmgroup/CheckoutCalculator/PriceCalculatorTest.java
@@ -1,4 +1,5 @@
 package com.fdmgroup.CheckoutCalculator;
+
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -15,46 +16,47 @@ import com.fdmgroup.util.PriceCalculator;
 public class PriceCalculatorTest {
 
 	private Basket basket;
-	
+
 	@Before
 	public void init() {
-		//adding items(fruits)
-		//should also enforce prices to be positive only	
-		Item apple1 = new Item(1,ItemTypeEnum.APPLE,2.0);
-		Item apple2 = new Item(2,ItemTypeEnum.APPLE,2.0);
-		Item banana = new Item(3,ItemTypeEnum.BANANA,1.0);
-		Item peach = new Item(4,ItemTypeEnum.PEACH,2.20);
-		Item orange = new Item(5,ItemTypeEnum.ORANGE,1.90);
-		Item lemon = new Item(6,ItemTypeEnum.LEMON,3.12);
-		List<Item> itemList = Arrays.asList(apple1,apple2,banana,peach,orange,lemon);
+		// adding items(fruits)
+		// should also enforce prices to be positive only
+		Item apple1 = new Item(1, ItemTypeEnum.APPLE, 2.0);
+		Item apple2 = new Item(2, ItemTypeEnum.APPLE, 2.0);
+		Item banana = new Item(3, ItemTypeEnum.BANANA, 1.0);
+		Item peach = new Item(4, ItemTypeEnum.PEACH, 2.20);
+		Item orange = new Item(5, ItemTypeEnum.ORANGE, 1.90);
+		Item lemon = new Item(6, ItemTypeEnum.LEMON, 3.12);
+		List<Item> itemList = Arrays.asList(apple1, apple2, banana, peach, orange, lemon);
 		basket = new Basket(itemList);
 	}
+
 	
 	
-	
+	  @Test public void test_price_calculation_of_empty_basket() { Basket basket =
+	  new Basket(); assertEquals(PriceCalculator.getBasketPrice(basket),0.0,0.002);
+	  }
+	 
+	 
 	@Test
-	public void test_price_calculation_of_empty_basket() {
-		Basket basket = new Basket();
-		assertEquals(PriceCalculator.getBasketPrice(basket),0.0,0.002);
+	public void test_price_calculation_of_basket__with_items_with_cheapest_being_free() {
+		// only one free
+		assertEquals(PriceCalculator.getBasketPrice(basket), 11.22, 0.002);
 	}
-	
-	@Test
-	public void test_price_calculation_of_basket__with_items() {
-		assertEquals(PriceCalculator.getBasketPrice(basket),12.22,0.002);
-	}
-	
-/* Unimplemented test case, but something that could also be considered.
-	@Test
-	public void test_price_calculation_of_basket__with_items_with_very_large_prices() {
-		Item orange = new Item(5,ItemTypeEnum.ORANGE,Integer.MAX_VALUE);
-		Item lemon = new Item(6,ItemTypeEnum.LEMON,Integer.MAX_VALUE);
-		
-		List<Item> itemList = Arrays.asList(orange,lemon);
-		basket = new Basket(itemList);
-		//should throw custom exception
-		PriceCalculator.getBasketPrice(basket);
-		
-	}
-*/
-	
+
+	/*
+	 * Unimplemented test case, but something that could also be considered.
+	 * 
+	 * @Test public void
+	 * test_price_calculation_of_basket__with_items_with_very_large_prices() { Item
+	 * orange = new Item(5,ItemTypeEnum.ORANGE,Integer.MAX_VALUE); Item lemon = new
+	 * Item(6,ItemTypeEnum.LEMON,Integer.MAX_VALUE);
+	 * 
+	 * List<Item> itemList = Arrays.asList(orange,lemon); basket = new
+	 * Basket(itemList); //should throw custom exception
+	 * PriceCalculator.getBasketPrice(basket);
+	 * 
+	 * }
+	 */
+
 }
